2bd5bdcb00258540f5ae417400dddc6f214b2c0f
czmaxis <czmaxis55@gmail.com>
2022-12-11 19:07:26 +0100

    přidání dalších požadovaných operací do metody main

--- src/Main.java
+++ src/Main.java
@@ -17,15 +17,15 @@ public class Main {
         }
 
 //Print list of states in format: state name, state shortcut, state Vat
-        System.out.println("List of States:\n");
+        System.out.println("Seznam států: \n");
 
         List<State> listOfStates = list.getListOfStates();
         for(State state : listOfStates){
             System.out.println(state.getName()+" ("+state.getShortcut()+") "+state.getHigherVat()+" %");
         }
 
-//Print list of states with VAT above 20% and haven´t special VAT in same format:
-System.out.println("\nStates with VAT above 20%:\n");
+//Print list of states with VAT above 20% wich haven´t special VAT in same format:
+System.out.println("\nStáty s DPH nad 20%:\n");
 
         for (State state : listOfStates){
             if (state.getHigherVat() > 20 && state.isHaveSpecialVat() == false){
@@ -33,8 +33,14 @@ System.out.println("\nStates with VAT above 20%:\n");
             }
         }
         Collections.sort(listOfStates, Collections.reverseOrder(new VatComparator()));
+        System.out.println("====================");
+        //Print shortcut´s of states from list with VAT 20% or lower and also with special VAT:
+        System.out.print("Sazba VAT 20 % nebo nižší nebo používají speciální sazbu: ");
+        for (State state : listOfStates){
+            if (state.getHigherVat() <= 20 && state.isHaveSpecialVat() == true){
+                System.out.print(state.getShortcut()+", ");
+            }
 
     }
-
-
-}
+    }
+    }
